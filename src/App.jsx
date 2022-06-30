c2188520b07bf710143d907cc5b592b0ff8ec16b
Armando Salazar <48894886+armandosalazar@users.noreply.github.com>
2022-06-30 16:18:54 -0500

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello Deploy!</h1>
+      <span>Test de deploy by Armando Salazar on GitHub Pages</span>
+    </div>
+  );
+}
+
+export default App;
